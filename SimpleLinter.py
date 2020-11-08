26900923f4c1edcdad1727f16c08f756be0acbf7
Matias Dogliani <matiasdogliani@hotmail.com>
2020-11-07 21:54:19 -0300

    Cambio listas por diccionario para llaves

--- SimpleLinter.py
+++ SimpleLinter.py
@@ -1,25 +1,20 @@
-openers = ['(', '{', '[']
-closers = [')', '}', ']']
-
+brackets = {')':'(', ']':'[', '}':'{'}
 acum = [] 
 
 inp = input() 
-inp = "{print(#hola)}" 
+
 #Guardo todos los que abren en la pila 
 for i in inp: 
-    if i in openers:
-        print(i)
+    if i in brackets.values():
         acum.append(i)
 
-print(acum) 
 
 for i in inp: 
-    if i in closers: 
-        if i == acum[len(acum) - 1]:  
+    if i in brackets.keys(): 
+        if brackets.get(i) == acum[len(acum) - 1]:  
             acum.pop() 
         else: 
             break; 
-print(acum) 
 if len(acum) ==0 :  
     print('True') 
 else:   
