04ba842ec1324ddd6bbd4fae1057e31fc5a72f3b
Matias Dogliani <matiasdogliani@hotmail.com>
2020-11-09 12:59:04 -0300

    Procesamiento de señal ya obtenida ECG

---
+++ ECGProcess.py
@@ -0,0 +1,21 @@
+
+
+''' 
+ECG Processing - Dogliani Matias, Toth Lautaro. 
+
+*Calcular los picos de la frecuencia cardiaca (signal.find_peaks) 
+*Calcular la frecuencia cardiaca bpm 
+*Determinar si dormia, reposaba o hacia actividad física 
+*Pedir sexo, edad 
+*Almacenar resultado en un archivo de texto.txt 
+
+'''
+
+
+import matplotlib as pl 
+import pandas as pd 
+import numpy as np 
+import scipy as sc 
+
+
+
