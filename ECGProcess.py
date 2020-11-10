717d4ef8b2f18eee300358d0f4c2ce328f2ac7fc
matias_dogliani <matiasdogliani@hotmail.com>
2020-11-09 23:46:14 -0300

    Actualizo pip

--- ECGProcess.py
+++ ECGProcess.py
@@ -1,21 +1,21 @@
 
 
-''' 
-ECG Processing - Dogliani Matias, Toth Lautaro. 
+'''
+ECG Processing - Dogliani Matias, Toth Lautaro.
 
-*Calcular los picos de la frecuencia cardiaca (signal.find_peaks) 
-*Calcular la frecuencia cardiaca bpm 
-*Determinar si dormia, reposaba o hacia actividad física 
-*Pedir sexo, edad 
-*Almacenar resultado en un archivo de texto.txt 
+*Calcular los picos de la frecuencia cardiaca (signal.find_peaks)
+*Calcular la frecuencia cardiaca bpm
+*Determinar si dormia, reposaba o hacia actividad física
+*Pedir sexo, edad
+*Almacenar resultado en un archivo de texto.txt
 
 '''
 
 
-import matplotlib as pl 
-import pandas as pd 
-import numpy as np 
-import scipy as sc 
+import pandas as pd
+import numpy as np
 
 
 
+data = read_excel('electrocardiograma.xlsx')
+print(data)
