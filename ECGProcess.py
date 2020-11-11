484921285c049fb461698048f7ad4971601d123a
matias_dogliani <matiasdogliani@hotmail.com>
2020-11-10 23:49:12 -0300

    Grafico puntos max y texto con bpm

--- ECGProcess.py
+++ ECGProcess.py
@@ -1,5 +1,4 @@
 
-
 '''
 ECG Processing - Dogliani Matias, Toth Lautaro.
 
@@ -10,21 +9,34 @@ ECG Processing - Dogliani Matias, Toth Lautaro.
 *Almacenar resultado en un archivo de texto.txt
 
 '''
-
-
+#Scipy puede generar un ECG arbitrariamente para probar con otros scipy.misc.electrocardiograma()
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 
 data = pd.read_excel('electrocardiograma.xlsx')
 
-print(data)
 TimeAxe = data['tiempo']
 SignalAxe = data['señal']
+picos,_ = find_peaks(SignalAxe, height=0.5, width = 10)
+
+
+#Como el eje x son segundos, el periodo es la cantidad de
+#segundos entre dos picos consecutivos
+#La frecuencia es la inversa. 1 Hz es un pulso en un segundo, que equivale
+#a 60 pulsos (latidos) en un minuto
+
+T = TimeAxe[picos[1]] -TimeAxe[picos[0]]
+freq = (1/T) * 60
+lpm = str(freq) + 'bpm'
+
 
 plt.plot(TimeAxe,SignalAxe,'g')
+plt.plot(TimeAxe[picos], SignalAxe[picos],'o', color = 'red')
+plt.grid()
+texto1 = plt.text(0.5,max(SignalAxe), lpm, fontsize=20)
+
 plt.title('ECG')
-
-
-
 plt.savefig('ECGSignal',dpi=300)
 plt.show()
