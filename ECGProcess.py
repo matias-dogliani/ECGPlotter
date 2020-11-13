f7429f9e38f58cec1a91df1e7abf908fc84bd747
matias_dogliani <matiasdogliani@hotmail.com>
2020-11-13 01:42:28 -0300

    Agrego la entrada de datos para clasificacion de estados

--- ECGProcess.py
+++ ECGProcess.py
@@ -10,14 +10,55 @@ ECG Processing - Dogliani Matias, Toth Lautaro.
 
 '''
 #Scipy puede generar un ECG arbitrariamente para probar con otros scipy.misc.electrocardiograma()
-import numpy as np
+import tkinter as tk
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 from scipy.signal import find_peaks
 
-data = pd.read_excel('electrocardiograma.xlsx')
 
+
+#Datos requeridos para el calculo de la frecuencia ideal y clasificacion de estado
+#Ingreso de datos y comprobacion
+
+Edad = 0
+Sexo = 'AA'
+Peso = 0
+
+while(Edad <= 0):
+    try:
+        Edad = int(input('Edad del paciente: ')) #Al utilizar try no se rompe
+                                                 # si no se ingresa un numero de base10
+    except:
+        Edad = 0
+    if Edad <= 0 or  Edad >= 120:
+        Edad = 0
+        print('Edad ingresada incorrecta, vuelva a ingresar')
+
+while(Peso <= 0):
+    try:
+        Peso = int(input('Ingreso el peso del paciente (Kg): '))
+    except:
+        Peso = 0
+    if Peso <= 0 or Peso >= 500:
+        Peso = 0
+        print('Edad ingresada incorrecta, vuelva a ingresar')
+
+while Sexo[0] != 'M' and  Sexo[0] != 'F':
+    Sexo = input('Ingrese el sexo del paciente (M)asculino, (F)emenino: ')
+    Sexo.upper()
+    if Sexo[0] != 'M' and Sexo[0] != 'F':
+        Sexo = 'AA'
+        print('Sexo ingresado incorrecto, vuelva a ingresar')
+
+
+#Calculo de frecuencia maxima  y clasificaicon de estados
+
+#hombre 	[(210 - (0,5 * edad en años)) - (0,01 * peso en kg) + 4]
+#mujer 	[(210 - (0,5 * edad en años)) - (0,01 * peso en kg)]
+#La frecuencia cardíaca en reposo depende de factores genéticos, del estado físico, del estado psicológico, de las condiciones ambientales, de la postura, de la edad y del sexo. Se toma generalmente con la persona en reposo: sentada, o acostada. En un adulto sano, en reposo, el pulso suele hallarse en el rango de los 60-100 latidos por minuto. Durante el ejercicio físico el rango puede aumentar a 150-200 latidos por minuto y durante el sueño puede bajar de 60 latidos por minuto.
+
+data = pd.read_excel('electrocardiograma.xlsx')
 TimeAxe = data['tiempo']
 SignalAxe = data['señal']
 picos,_ = find_peaks(SignalAxe, height=0.5, width = 10)
